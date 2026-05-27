65f67bd7e032a32c325a641db53d02fe1bb75ff0
Jamik <dzamikhidirov8@gmail.com>
2026-05-28 02:26:43 +0300

    Rename project to SuperBot

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = "7042929053:AAEsz4mIBA6P2ZKoPRiMuad1UIdR8dS9TQE"
 OWNER_ID = 1819756249
 ADMINS = [OWNER_ID]
 VERSION = "7.0.0"
-PROJECT_NAME = "CasinoBot"
+PROJECT_NAME = "SuperBot"
 DB_NAME = "chat.db"
 
 # OSINT API Keys (оставьте пустыми — функции будут работать без ключей, но с ограничениями)
